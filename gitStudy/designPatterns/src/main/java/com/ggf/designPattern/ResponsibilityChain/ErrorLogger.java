149f5a5e82dc5bd07d0b836113321342baf9dfb6
Elegant <736065863@qq.com>
2021-05-08 15:42:08 +0800

    添加责任链模式

---
+++ gitStudy/designPatterns/src/main/java/com/ggf/designPattern/ResponsibilityChain/ErrorLogger.java
@@ -0,0 +1,13 @@
+package com.ggf.designPattern.ResponsibilityChain;
+
+public class ErrorLogger extends AbstractLogger{
+
+    public ErrorLogger(int level) {
+        this.level = level;
+    }
+
+    @Override
+    protected void wirte(String message) {
+        System.out.println("ERROR::Logger" + message);
+    }
+}
