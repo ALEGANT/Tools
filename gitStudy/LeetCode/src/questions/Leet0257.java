5b6b12dd8bb947a56eae1bcc0bf5f32398f601f8
Elegant <736065863@qq.com>
2022-02-12 16:43:47 +0800

    改名

--- gitStudy/LeetCode/src/questions/Leet0257.java
+++ gitStudy/LeetCode/src/questions/Leet0257.java
@@ -1,19 +1,4 @@
-/**
- * Definition for a binary tree node.
- * public class TreeNode {
- *     int val;
- *     TreeNode left;
- *     TreeNode right;
- *     TreeNode() {}
- *     TreeNode(int val) { this.val = val; }
- *     TreeNode(int val, TreeNode left, TreeNode right) {
- *         this.val = val;
- *         this.left = left;
- *         this.right = right;
- *     }
- * }
- */
-class Solution {
+class Leet0257 {
     public List<String> binaryTreePaths(TreeNode root) {
         List<String> list = new ArrayList<>();
         if(root == null) return list;
